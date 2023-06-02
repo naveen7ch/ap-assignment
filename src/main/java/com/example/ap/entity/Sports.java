cba2e31e10b56b52f3b5c229f76af556fb3c31f9
Naveen <xyz192529@gmail.com>
2023-06-02 09:49:52 +0530

    sports assiociated with no players. Test cases

--- src/main/java/com/example/ap/entity/Sports.java
+++ src/main/java/com/example/ap/entity/Sports.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Set;
 
